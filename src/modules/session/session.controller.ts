d3aac02a375f4799b8a2bbd3c65c5846efc5f1d3
emilianooferreyra <emilianooferreyra@gmail.com>
2026-06-05 23:13:55 -0300

    feat(sessions): add endpoints to list and revoke active sessions

--- src/modules/session/session.controller.ts
+++ src/modules/session/session.controller.ts
@@ -1,7 +1,27 @@
-import { Controller } from '@nestjs/common';
-import { SessionService } from './session.service';
+import { Controller, Delete, Get, HttpCode, HttpStatus, Param, UseGuards } from "@nestjs/common";
+import { SessionService } from "./session.service";
+import { JwtAuthGuard } from "../auth/guards/jwt-auth.guard";
+import { CurrentUser } from "../auth/decorators/current-user.decorator";
 
-@Controller('session')
+@Controller("sessions")
+@UseGuards(JwtAuthGuard)
 export class SessionController {
   constructor(private readonly sessionService: SessionService) {}
+
+  @Get()
+  async getAll(@CurrentUser() user) {
+    return this.sessionService.getAll({ userId: user.userId });
+  }
+
+  @Delete(":id")
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async deleteOne(@CurrentUser() user, @Param("id") id: string) {
+    return this.sessionService.delete({ id, userId: user.userId });
+  }
+
+  @Delete()
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async deleteAll(@CurrentUser() user) {
+    return this.sessionService.deleteAll({ userId: user.userId });
+  }
 }
--- src/modules/session/session.module.ts
+++ src/modules/session/session.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
+import { PassportModule } from "@nestjs/passport";
 import { SessionService } from "./session.service";
 import { SessionController } from "./session.controller";
 import { PrismaModule } from "../prisma/prisma.module";
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, PassportModule],
   controllers: [SessionController],
   providers: [SessionService],
   exports: [SessionService],
